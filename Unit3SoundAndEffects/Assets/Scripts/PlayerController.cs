947e32375a6a9fa9a139e9b9d5ac8df9950f9756
hyper-sonic1 <737509@student.dusd.net>
2024-04-12 15:30:27 -0700

    i added 2 new scripts
    and an obstacle

--- Unit3SoundAndEffects/Assets/Scripts/PlayerController.cs
+++ Unit3SoundAndEffects/Assets/Scripts/PlayerController.cs
@@ -8,14 +8,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
-   
+    public bool isOnGround = true;
     
+
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
     }
-    public bool isOnGround = true;
+    
 
     void Update()
     {
@@ -24,10 +26,11 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
             isOnGround = false;
 
-            private void OnCollisionEnter(Collision collision)
-            {
-                isOnGround = true;
-            }
         }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
