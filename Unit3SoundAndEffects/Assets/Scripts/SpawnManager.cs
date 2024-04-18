48b3b777f5d66ad625babc4ae8018e80275642bf
hyper-sonic1 <737509@student.dusd.net>
2024-04-18 10:09:34 -0700

    i added jump and am working
    on animation

--- Unit3SoundAndEffects/Assets/Scripts/SpawnManager.cs
+++ Unit3SoundAndEffects/Assets/Scripts/SpawnManager.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefabs;
+    public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefabs, spawnPos,  obstaclePrefabs.transform.rotation);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+    void SpawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+        
     }
 
     // Update is called once per frame
@@ -17,4 +29,5 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+
 }
