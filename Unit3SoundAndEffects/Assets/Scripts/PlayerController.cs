fefe0f14f4992eb3692c7723f2c2e81da5266862
hyper-sonic1 <737509@student.dusd.net>
2024-04-18 11:45:44 -0700

    I on last parrt
    almost done

--- Unit3SoundAndEffects/Assets/Scripts/PlayerController.cs
+++ Unit3SoundAndEffects/Assets/Scripts/PlayerController.cs
@@ -9,23 +9,25 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
-    public bool gameOver = false;   
-
+    public bool gameOver = false;
+    private Animator playerAnim;
+    public ParticleSystem explotionPartical;
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
     
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
             isOnGround = false;
-
+            playerAnim.SetTrigger("Jump_trig");
         }
         
     }
@@ -39,6 +41,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
