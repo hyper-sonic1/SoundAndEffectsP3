12fa6e066d2f7150e95123cd2ac2750ce5c62a0b
hyper-sonic1 <737509@student.dusd.net>
2024-04-26 12:20:06 -0700

    im finishing
    dashing

--- Unit3SoundAndEffects/Assets/Scripts/PlayerController.cs
+++ Unit3SoundAndEffects/Assets/Scripts/PlayerController.cs
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
     private AudioSource playerAudio;
     public bool doubleJumpUsed = false;
     public float doubleJumpForce;
+    public bool doubleSpeed = false;
 
     void Start()
     {
@@ -35,21 +36,30 @@ public class PlayerController : MonoBehaviour
         {
             playerAudio.PlayOneShot(jumpSound, 1.0f);
             dirtPartical.Stop();
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
 
             doubleJumpUsed = false;
-            { 
-                else if(Input.GetKeyDown(KeyCode.Space)&&!isOnGround&&!doubleJumpUsed)
-                {
-                    doubleJumpUsed = true;
-                    playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
-                    playerAnim.Play("Running_Jump", 3, 0f);
-                    playerAudio.PlayOneShot(jumpSound, 1.0f);
-                }
-            }
         }
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3, 0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            doubleSpeed = true;
+            playerAnim.SetFloat("Speed_Mulriplier", 2.0f);
+        }
+        else if(doubleSpeed)
+        {
+            doubleSpeed = false;
+            playerAnim.SetFloat("Speed_Multiplier", 1.0f);
+        }
+        
         
     }
     private void OnCollisionEnter(Collision collision)
